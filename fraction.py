25f249b70993aa80a505a13e2fe546a46c3d08ec
BautiG <juanbautistagallino@hanovernorwichschools.org>
2018-10-22 16:20:38 -0400

    Updated from Brython Server: 10/22/2018 4:21:11 PM

--- fraction.py
+++ fraction.py
@@ -1,23 +1,27 @@
 import math
 #please only imput simplified fractions :)
-
-nuMerator = int(input("what is your numerator? "))
+nuMerator = 1
+#nuMerator = int(input("what is your numerator? "))
 deNominator = int(input("what is your denominator? "))
 
 nuMbers = []
 
 nonRepeat = 0
 
-while deNominator%2 == 0 or deNominator%5 == 0 or deNominator%10 == 0
-if deNominator%2 == 0:
-    deNominator = deNominator/2
-    
-if deNominator%5 == 0:
-    deNominator = deNominator/5
-    
-if deNominator%10 == 0:
-    deNominator = deNominator/10
-    
-    nonRepeat += 1
+while deNominator%2 == 0 or deNominator%5 == 0 or deNominator%10 == 0:
+    if deNominator%2 == 0:
+        deNominator = deNominator/2
+        nonRepeat += 1
+    if deNominator%5 == 0:
+        deNominator = deNominator/5
+        nonRepeat += 1
+    if deNominator%10 == 0:
+        deNominator = deNominator/10
+        nonRepeat += 1
 
-print(deNominator)
+
+print(nonRepeat)
+
+rePeat = 0
+
+aNswer = nuMerator/deNominator
