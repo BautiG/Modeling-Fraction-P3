38401e58c79c519899a5581078d3d461ff4d1827
BautiG <juanbautistagallino@hanovernorwichschools.org>
2018-10-22 15:49:03 -0400

    Updated from Brython Server: 10/22/2018 3:49:02 PM

--- fraction.py
+++ fraction.py
@@ -1,5 +1,15 @@
 import math
+#please only imput simplified fractions :)
 
-nuMerator = int(input("what is your numerator"))
-deNominator = int(input("what is your denominator?"))
+nuMerator = int(input("what is your numerator? "))
+deNominator = int(input("what is your denominator? "))
 
+reMain = nuMerator%deNominator
+
+print(reMain)
+"""
+if reMain == 0:
+    print(1)
+elif:
+    print(2)
+"""
